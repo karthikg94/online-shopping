2b6af08b86c867a37fca312bea3e6e293153df6d
karthik <karthikg940@gmail.com>
2019-10-25 22:18:26 +0530

    Successfully started and created home page with bootstrap

---
+++ onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
@@ -0,0 +1,21 @@
+package net.kzn.onlineshopping.controller;
+
+import javax.websocket.server.PathParam;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	
+	@RequestMapping(value = {"/","/index","/home"})
+	public ModelAndView index(){
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting","Welcome");
+		return mv;
+	}
+	
+}
